c944ac7ca46114f1e5dc879d39da640758d07498
Kristijan Karanfiloski <kikaccc28@gmail.com>
2023-04-09 13:36:08 +0200

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+// function countInital() {
+//   console.log("run function");
+//   return 4;
+// }
 
 function App() {
+  // const [count, setCount] = useState(countInital());
+
+  // const [count, setCount] = useState(() => {
+  //   console.log("run function");
+  //   return 4;
+  // });
+
+  // const [state, setState] = useState({ count: 4, theme: "blue" });
+  // const count = state.count;
+  // const theme = state.theme;
+
+  const [count, setCount] = useState(4);
+  const [theme, setTheme] = useState("blue");
+
+  function decrementCount() {
+    setCount((prevCount) => prevCount - 1);
+    // setState((prevState) => {
+    //   return { ...prevState, count: prevState.count - 1 };
+    // });
+    setTheme("red");
+  }
+
+  function incrementCount() {
+    setCount((prevCount) => prevCount + 1);
+    setTheme("blue");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center">
+      <button onClick={decrementCount} className="minus">
+        -
+      </button>
+      <span>{count}</span>
+      <span>{theme}</span>
+      <button onClick={incrementCount} className="plus">
+        +
+      </button>
     </div>
   );
 }
